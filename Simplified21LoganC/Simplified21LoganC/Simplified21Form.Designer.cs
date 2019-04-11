4df76f51094813529dc9f11697ac45f50e86e469
Logan Cantin <logan.cantin@gmail.com>
2019-04-10 23:09:35 -0400

    Simplified 21

---
+++ Simplified21LoganC/Simplified21LoganC/Simplified21Form.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace Simplified21LoganC
+{
+    partial class frmSimplified21
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstPlayerHand = new System.Windows.Forms.ListBox();
+            this.lstDealerCards = new System.Windows.Forms.ListBox();
+            this.grbDealer = new System.Windows.Forms.GroupBox();
+            this.lblDealerSum = new System.Windows.Forms.Label();
+            this.lblDealerScore = new System.Windows.Forms.Label();
+            this.grbPlayer = new System.Windows.Forms.GroupBox();
+            this.lblSplitSum = new System.Windows.Forms.Label();
+            this.lstPlayerSplitHand = new System.Windows.Forms.ListBox();
+            this.lblUserSum = new System.Windows.Forms.Label();
+            this.lblUserScore = new System.Windows.Forms.Label();
+            this.btnStay = new System.Windows.Forms.Button();
+            this.btnSplit = new System.Windows.Forms.Button();
+            this.btnHit = new System.Windows.Forms.Button();
+            this.mnuMenu = new System.Windows.Forms.MenuStrip();
+            this.mniFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniNewGame = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.grbDealer.SuspendLayout();
+            this.grbPlayer.SuspendLayout();
+            this.mnuMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lstPlayerHand
+            // 
+            this.lstPlayerHand.FormattingEnabled = true;
+            this.lstPlayerHand.Location = new System.Drawing.Point(7, 19);
+            this.lstPlayerHand.Name = "lstPlayerHand";
+            this.lstPlayerHand.Size = new System.Drawing.Size(184, 95);
+            this.lstPlayerHand.TabIndex = 0;
+            // 
+            // lstDealerCards
+            // 
+            this.lstDealerCards.FormattingEnabled = true;
+            this.lstDealerCards.Location = new System.Drawing.Point(6, 20);
+            this.lstDealerCards.Name = "lstDealerCards";
+            this.lstDealerCards.Size = new System.Drawing.Size(184, 95);
+            this.lstDealerCards.TabIndex = 1;
+            // 
+            // grbDealer
+            // 
+            this.grbDealer.Controls.Add(this.lblDealerSum);
+            this.grbDealer.Controls.Add(this.lblDealerScore);
+            this.grbDealer.Controls.Add(this.lstDealerCards);
+            this.grbDealer.Location = new System.Drawing.Point(13, 41);
+            this.grbDealer.Name = "grbDealer";
+            this.grbDealer.Size = new System.Drawing.Size(369, 172);
+            this.grbDealer.TabIndex = 2;
+            this.grbDealer.TabStop = false;
+            this.grbDealer.Text = "Dealer";
+            // 
+            // lblDealerSum
+            // 
+            this.lblDealerSum.AutoSize = true;
+            this.lblDealerSum.Location = new System.Drawing.Point(211, 46);
+            this.lblDealerSum.Name = "lblDealerSum";
+            this.lblDealerSum.Size = new System.Drawing.Size(35, 13);
+            this.lblDealerSum.TabIndex = 3;
+            this.lblDealerSum.Text = "label1";
+            // 
+            // lblDealerScore
+            // 
+            this.lblDealerScore.AutoSize = true;
+            this.lblDealerScore.Location = new System.Drawing.Point(208, 29);
+            this.lblDealerScore.Name = "lblDealerScore";
+            this.lblDealerScore.Size = new System.Drawing.Size(81, 13);
+            this.lblDealerScore.TabIndex = 2;
+            this.lblDealerScore.Text = "Dealer Score: 0";
+            // 
+            // grbPlayer
+            // 
+            this.grbPlayer.Controls.Add(this.lblSplitSum);
+            this.grbPlayer.Controls.Add(this.lstPlayerHand);
+            this.grbPlayer.Controls.Add(this.lstPlayerSplitHand);
+            this.grbPlayer.Controls.Add(this.lblUserSum);
+            this.grbPlayer.Controls.Add(this.lblUserScore);
+            this.grbPlayer.Controls.Add(this.btnStay);
+            this.grbPlayer.Controls.Add(this.btnSplit);
+            this.grbPlayer.Controls.Add(this.btnHit);
+            this.grbPlayer.Location = new System.Drawing.Point(13, 219);
+            this.grbPlayer.Name = "grbPlayer";
+            this.grbPlayer.Size = new System.Drawing.Size(369, 198);
+            this.grbPlayer.TabIndex = 3;
+            this.grbPlayer.TabStop = false;
+            this.grbPlayer.Text = "Player";
+            // 
+            // lblSplitSum
+            // 
+            this.lblSplitSum.AutoSize = true;
+            this.lblSplitSum.Location = new System.Drawing.Point(108, 117);
+            this.lblSplitSum.Name = "lblSplitSum";
+            this.lblSplitSum.Size = new System.Drawing.Size(57, 13);
+            this.lblSplitSum.TabIndex = 7;
+            this.lblSplitSum.Text = "Split Sum: ";
+            // 
+            // lstPlayerSplitHand
+            // 
+            this.lstPlayerSplitHand.FormattingEnabled = true;
+            this.lstPlayerSplitHand.Location = new System.Drawing.Point(117, 19);
+            this.lstPlayerSplitHand.Name = "lstPlayerSplitHand";
+            this.lstPlayerSplitHand.Size = new System.Drawing.Size(74, 95);
+            this.lstPlayerSplitHand.TabIndex = 6;
+            this.lstPlayerSplitHand.Visible = false;
+            // 
+            // lblUserSum
+            // 
+            this.lblUserSum.AutoSize = true;
+            this.lblUserSum.Location = new System.Drawing.Point(6, 117);
+            this.lblUserSum.Name = "lblUserSum";
+            this.lblUserSum.Size = new System.Drawing.Size(56, 13);
+            this.lblUserSum.TabIndex = 5;
+            this.lblUserSum.Text = "Your Sum:";
+            // 
+            // lblUserScore
+            // 
+            this.lblUserScore.AutoSize = true;
+            this.lblUserScore.Location = new System.Drawing.Point(207, 36);
+            this.lblUserScore.Name = "lblUserScore";
+            this.lblUserScore.Size = new System.Drawing.Size(72, 13);
+            this.lblUserScore.TabIndex = 4;
+            this.lblUserScore.Text = "Your Score: 0";
+            // 
+            // btnStay
+            // 
+            this.btnStay.Location = new System.Drawing.Point(168, 154);
+            this.btnStay.Name = "btnStay";
+            this.btnStay.Size = new System.Drawing.Size(75, 23);
+            this.btnStay.TabIndex = 3;
+            this.btnStay.Text = "Stay";
+            this.btnStay.UseVisualStyleBackColor = true;
+            this.btnStay.Click += new System.EventHandler(this.btnStay_Click);
+            // 
+            // btnSplit
+            // 
+            this.btnSplit.Location = new System.Drawing.Point(87, 154);
+            this.btnSplit.Name = "btnSplit";
+            this.btnSplit.Size = new System.Drawing.Size(75, 23);
+            this.btnSplit.TabIndex = 2;
+            this.btnSplit.Text = "Split";
+            this.btnSplit.UseVisualStyleBackColor = true;
+            this.btnSplit.Click += new System.EventHandler(this.btnSplit_Click);
+            // 
+            // btnHit
+            // 
+            this.btnHit.Location = new System.Drawing.Point(6, 154);
+            this.btnHit.Name = "btnHit";
+            this.btnHit.Size = new System.Drawing.Size(75, 23);
+            this.btnHit.TabIndex = 1;
+            this.btnHit.Text = "Hit";
+            this.btnHit.UseVisualStyleBackColor = true;
+            this.btnHit.Click += new System.EventHandler(this.btnHit_Click);
+            // 
+            // mnuMenu
+            // 
+            this.mnuMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniFile});
+            this.mnuMenu.Location = new System.Drawing.Point(0, 0);
+            this.mnuMenu.Name = "mnuMenu";
+            this.mnuMenu.Size = new System.Drawing.Size(396, 24);
+            this.mnuMenu.TabIndex = 4;
+            this.mnuMenu.Text = "menuStrip1";
+            // 
+            // mniFile
+            // 
+            this.mniFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniNewGame,
+            this.mniExit});
+            this.mniFile.Name = "mniFile";
+            this.mniFile.Size = new System.Drawing.Size(37, 20);
+            this.mniFile.Text = "File";
+            // 
+            // mniNewGame
+            // 
+            this.mniNewGame.Name = "mniNewGame";
+            this.mniNewGame.Size = new System.Drawing.Size(132, 22);
+            this.mniNewGame.Text = "New Game";
+            this.mniNewGame.Click += new System.EventHandler(this.restartToolStripMenuItem_Click);
+            // 
+            // mniExit
+            // 
+            this.mniExit.Name = "mniExit";
+            this.mniExit.Size = new System.Drawing.Size(132, 22);
+            this.mniExit.Text = "Exit";
+            this.mniExit.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(94, 153);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(162, 115);
+            this.btnPlay.TabIndex = 5;
+            this.btnPlay.Text = "Play Blackjack!";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // frmSimplified21
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(396, 439);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.grbPlayer);
+            this.Controls.Add(this.grbDealer);
+            this.Controls.Add(this.mnuMenu);
+            this.MainMenuStrip = this.mnuMenu;
+            this.Name = "frmSimplified21";
+            this.Text = "Simplified 21 by Logan Cantin";
+            this.grbDealer.ResumeLayout(false);
+            this.grbDealer.PerformLayout();
+            this.grbPlayer.ResumeLayout(false);
+            this.grbPlayer.PerformLayout();
+            this.mnuMenu.ResumeLayout(false);
+            this.mnuMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstPlayerHand;
+        private System.Windows.Forms.ListBox lstDealerCards;
+        private System.Windows.Forms.GroupBox grbDealer;
+        private System.Windows.Forms.Label lblDealerScore;
+        private System.Windows.Forms.GroupBox grbPlayer;
+        private System.Windows.Forms.Label lblUserScore;
+        private System.Windows.Forms.Button btnStay;
+        private System.Windows.Forms.Button btnSplit;
+        private System.Windows.Forms.Button btnHit;
+        private System.Windows.Forms.Label lblUserSum;
+        private System.Windows.Forms.Label lblDealerSum;
+        private System.Windows.Forms.ListBox lstPlayerSplitHand;
+        private System.Windows.Forms.Label lblSplitSum;
+        private System.Windows.Forms.MenuStrip mnuMenu;
+        private System.Windows.Forms.ToolStripMenuItem mniFile;
+        private System.Windows.Forms.ToolStripMenuItem mniNewGame;
+        private System.Windows.Forms.ToolStripMenuItem mniExit;
+        private System.Windows.Forms.Button btnPlay;
+    }
+}
+
---
+++ Simplified21LoganC/Simplified21LoganC/Simplified21Form.cs
@@ -0,0 +1,404 @@
+﻿/*
+ * Created by: Logan Cantin
+ * Created on: April 10th, 2019
+ * Created for: ICS3U Programming
+ * Assignment #6b - Simplified 21
+ * This program allows the user to play a simplified version of the popular casino game 21.
+ * 
+ * Bonus features:
+ * - Scores
+ * - Split feature (really complicated - should be worth 2 marks)
+ * - Running sums
+ * - hit more than once
+ * - Intelligent dealer
+ * - Sounds
+ * - pass by reference function (Deal)
+ * - play button
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Media;
+
+namespace Simplified21LoganC
+{
+
+    public partial class frmSimplified21 : Form
+    {
+        // Variables
+        Random randomGenerator = new Random();
+        int dealerScore = 0;
+        int userScore = 0;
+        int userSplitSum = 0;
+        bool split = false;
+        int activeHand = 1;
+        int userSum, dealerSum;
+        SoundPlayer cheer = new SoundPlayer(Properties.Resources.cheer);
+        SoundPlayer aww = new SoundPlayer(Properties.Resources.aww);
+        
+        // Deals (numCards) cards to the supplied listBox
+        void Deal(ref ListBox listBox, int numCards)
+        {
+            // Adds (numCards) to the listbox
+            for (int i = numCards; i > 0; i--)
+                 listBox.Items.Add(randomGenerator.Next(1, 11));
+
+            // Calculates sum of the cards in hand
+            int sum = 0;
+            foreach (int card in listBox.Items)
+            {
+                sum += card;
+            }
+
+            // Updates the appropriate sum label
+            if (listBox == lstPlayerHand)
+            {
+                userSum = sum;
+                lblUserSum.Text = "Your sum: " + Convert.ToString(userSum);
+            }
+            else if (listBox == lstDealerCards)
+            {
+                dealerSum = sum;
+                lblDealerSum.Text = "Dealer sum: " + Convert.ToString(dealerSum);
+            }
+            else if (listBox == lstPlayerSplitHand)
+            {
+                userSplitSum = sum;
+                lblSplitSum.Text = "Split Sum: " + Convert.ToString(userSplitSum);
+            }
+        }
+
+        // Start a new round
+        void Setup()
+        {
+            // Clear all hands. Resize the player's hand to the original size
+            lstDealerCards.Items.Clear();
+            lstPlayerHand.Items.Clear();
+            lstPlayerSplitHand.Items.Clear();
+            lstPlayerHand.Size = new Size(184, 95);
+
+            // Resets split
+            split = false;
+            btnSplit.Enabled = false;
+            lblSplitSum.Hide();
+
+            // Resets the sum texts, and hides the 
+            lblDealerSum.Text = "Dealer Sum: ";
+            lblUserSum.Text = "Your Sum: ";
+
+            //Deals 2 cards to the user and 1 to the dealer
+            Deal(ref lstDealerCards, 1);
+            Deal(ref lstPlayerHand, 2);
+
+            //Let the player have their turn
+            PlayerTurn();
+        }
+
+        // Starts player's turn
+        void PlayerTurn()
+        {
+            // Enables player controls, disables dealer
+            grbDealer.Enabled = false;
+            grbPlayer.Enabled = true;
+
+            // If the player's cards are the same, allow them to split
+            if ((int)lstPlayerHand.Items[0] == (int)lstPlayerHand.Items[1])
+            {
+                btnSplit.Enabled = true;
+            }
+        }
+
+        // Start dealer's turn and make plays for dealer
+        void DealerTurn()
+        {
+            // Enable dealer, disable player
+            grbDealer.Enabled = true;
+            grbPlayer.Enabled = false;
+            
+            // Keep hitting until dealer's hand has a sum of at least 17
+            while (dealerSum < 17)
+            {
+                Deal(ref lstDealerCards, 1);
+            }
+
+            // Determine the winner of the round
+            DetermineWinner();
+        }
+
+        void DealerWin()
+        {
+            dealerScore++;
+            aww.Load();
+            aww.Play();
+        }
+
+        void UserWin()
+        {
+            userScore++;
+            cheer.Load();
+            cheer.Play();
+        }
+
+        // Determines winner(s) of the round
+        void DetermineWinner()
+        {
+            // If the player split their hand
+            if (split)
+            {
+                // HAND 1 RESULT
+                if (userSum > 21)
+                {
+                    DealerWin();
+                    MessageBox.Show("Hand 1: Bust, Dealer Win!");
+                    
+                }
+                else if (userSum == 21)
+                {
+                    UserWin();
+                    MessageBox.Show("Hand 1: Blackjack! You win!");
+                }
+                else if (dealerSum > 21)
+                {
+                    UserWin();
+                    MessageBox.Show("Hand 1: Dealer bust, you win!");
+                }
+                else if (userSum == dealerSum)
+                {
+                    MessageBox.Show("Hand 1: Tie");
+                }
+                else if (userSum > dealerSum)
+                {
+                    UserWin();
+                    MessageBox.Show("Hand 1: You beat the dealer");
+                }
+                else if (dealerSum > userSum)
+                {
+                    DealerWin();
+                    MessageBox.Show("Hand 1: The dealer beat you");
+                }
+                else
+                {
+                    MessageBox.Show("Hand 1: ERROR");
+                }
+
+
+                // HAND 2 RESULT
+                if (userSplitSum > 21)
+                {
+                    DealerWin();
+                    MessageBox.Show("Hand 2 bust, Dealer Win!");
+                    
+                }
+                else if (userSplitSum == 21)
+                {
+                    UserWin();
+                    MessageBox.Show("Hand 2 Blackjack! You win!");
+                    
+                }
+                else if (dealerSum > 21)
+                {
+                    UserWin();
+                    MessageBox.Show("Hand 2: Dealer bust, you win!");
+                    
+                }
+                else if (userSplitSum == dealerSum)
+                {
+                    MessageBox.Show("Hand 2: Tie");
+                }
+                else if (userSplitSum > dealerSum)
+                {
+                    UserWin();
+                    MessageBox.Show("Hand 2: You beat the dealer");
+                    
+                }
+                else if (dealerSum > userSplitSum)
+                {
+                    DealerWin();
+                    MessageBox.Show("Hand 2: The dealer beat you");
+                    
+                }
+                else
+                {
+                    MessageBox.Show("Hand 2: ERROR");
+                }
+            }
+
+            // No split - only one hand
+            else
+            {
+                if (userSum > 21)
+                {
+                    DealerWin();
+                    MessageBox.Show("You bust, Dealer Win!");
+                }
+                else if (userSum == 21)
+                {
+                    UserWin();
+                    MessageBox.Show("Blackjack! You win!");
+                    
+                }
+                else if (dealerSum > 21)
+                {
+                    UserWin();
+                    MessageBox.Show("Dealer bust, you win!");
+                    
+                }
+                else if (userSum == dealerSum)
+                {
+                    MessageBox.Show("Tie");
+                }
+                else if (userSum > dealerSum)
+                {
+                    UserWin();
+                    MessageBox.Show("You beat the dealer");
+                    
+                }
+                else if (dealerSum > userSum)
+                {
+                    DealerWin();
+                    MessageBox.Show("The dealer beat you");
+                    
+                }
+                else
+                {
+                    MessageBox.Show("ERROR");
+                }
+            }
+            
+            // Update user and dealer's scores
+            lblUserScore.Text = "Your score: " + Convert.ToString(userScore);
+            lblDealerScore.Text = "Dealer's Score: " + Convert.ToString(dealerScore);
+
+            // New round
+            Setup();
+        }
+
+        // Hit procedure for player
+        private void btnHit_Click(object sender, EventArgs e)
+        {
+            // Player can't split anymore
+            btnSplit.Enabled = false;
+            
+            // If the user's split hand is active
+            if (activeHand == 2)
+            {
+                // Deal a card to the player's split hand. If blackjack or bust, move on to other hand
+                Deal(ref lstPlayerSplitHand, 1);
+                if (userSplitSum > 20)
+                    btnStay_Click(this, new EventArgs());
+                
+            }
+
+            // Regular hand is active
+            else
+            {
+                // Deal a card to the player's regular hand. If blackjack or bust, move on to dealer's turn
+                Deal(ref lstPlayerHand, 1);
+                if (userSum > 20)
+                    DealerTurn();
+            }
+        }
+
+        // Stay procedure for player
+        private void btnStay_Click(object sender, EventArgs e)
+        {
+            // Disable split button
+            btnSplit.Enabled = false;
+
+            // If split hand is active
+            if (activeHand == 2)
+            {
+                // Enable regular hand and disable split hand. Move active hand over to regular hand (1)
+                lstPlayerHand.Enabled = true;
+                lstPlayerSplitHand.Enabled = false;
+                activeHand--;
+            }
+
+            // Regular hand
+            else
+            {
+                // Move on to dealer's turn
+                DealerTurn();
+            }            
+        }
+        
+        // Split procedure for player
+        private void btnSplit_Click(object sender, EventArgs e)
+        {
+            // Make the regular player hand smaller to accomodate for the split hand. Show split hand
+            lstPlayerHand.Size = new Size(75, 95);
+            lstPlayerSplitHand.Show();
+
+            // Show split sum
+            lblSplitSum.Show();
+
+            // Set active hand to split. Set split to true.
+            activeHand = 2;
+            split = true;
+
+            // Enable split hand and disable regular hand
+            lstPlayerSplitHand.Enabled = true;
+            lstPlayerHand.Enabled = false;
+
+            // Move 1 card from regular hand over to split hand
+            lstPlayerSplitHand.Items.Add((int)lstPlayerHand.Items[0]);
+            lstPlayerHand.Items.RemoveAt(1);
+
+            // New sums
+            lblUserSum.Text = "Your sum: " + Convert.ToString((int)lstPlayerHand.Items[0]);
+            lblSplitSum.Text = "Split sum: " + Convert.ToString((int)lstPlayerHand.Items[0]);
+
+            //Disable split button
+            btnSplit.Enabled = false;
+        }
+
+        // New game
+        private void restartToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Reset scores. Start new round
+            userScore = 0;
+            dealerScore = 0;
+
+            // Reset labels
+            lblDealerScore.Text = "Dealer Score: 0";
+            lblUserScore.Text = "Your Score: 0";
+
+            grbDealer.Hide();
+            grbPlayer.Hide();
+            btnPlay.Show();
+        }
+
+        // Exit game
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        // Start game
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            //Show all controls, start new round
+            grbDealer.Show();
+            grbPlayer.Show();
+            btnPlay.Hide();
+            Setup();
+        }
+
+        public frmSimplified21()
+        {
+            InitializeComponent();
+
+            //Hide everything except play button
+            grbDealer.Hide();
+            grbPlayer.Hide();
+            btnPlay.Show();
+        }
+    }
+}
