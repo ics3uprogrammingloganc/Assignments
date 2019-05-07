0d474bb2d65961c4b0c4f8bf783683567a83a0af
loganschool <logan.cantin@ocsbstudent.ca>
2019-05-07 13:08:29 -0400

    Assignment 7

---
+++ Better21LoganC/Better21LoganC/Better21.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Better21LoganC
+{
+    static class Better21
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmBetter21());
+        }
+    }
+}
